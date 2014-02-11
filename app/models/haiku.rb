5e3c993f0c9b03462550e7a7a83f16bb5d76ae3d
LOZORD <ljrudberg@gmail.com>
2014-02-10 22:09:25 -0600

    Finish demo app

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,4 +1,5 @@
 class Haiku < ActiveRecord::Base
+	belongs_to :user
 	validates :content, presence: true, length: {in: 10..100, too_long: "Your haiku seems too long...", too_short: "Your haiku seems too short..."}
 	validates :author, presence: true
 	#validates something with user_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :haikus
 	validates :name, presence: true, uniqueness: true
 	validates :email, presence: true, uniqueness: true
 end
