6af0e860cbefe97324ebe20b26c4c85a7b3b8e6b
LOZORD <ljrudberg@gmail.com>
2014-02-10 21:45:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Haikooky::Application.config.secret_key_base = 'f6e8027cc5e5173b9a573b40e73c35ffab8592f56b1368b41cc6a8611d43c2e448460c933ef2fb6212fd9632c966228488f309da075f0a645c6c0432485e7d4b'
