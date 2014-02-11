cc7bd36de5082a87f8d480bbb40147118ad747db
LOZORD <ljrudberg@gmail.com>
2014-02-10 22:05:59 -0600

    Added validations to models

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,2 +1,5 @@
 class Haiku < ActiveRecord::Base
+	validates :content, presence: true, length: {in: 10..100, too_long: "Your haiku seems too long...", too_short: "Your haiku seems too short..."}
+	validates :author, presence: true
+	#validates something with user_id
 end
